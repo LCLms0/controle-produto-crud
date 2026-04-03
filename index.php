282d489c5f36df5ae64840145426919566fef040
Lucas Lemos <lucaslemoss608@gmail.com>
2026-04-03 12:11:46 -0300

    fznd

--- index.php
+++ index.php
@@ -1,8 +1,17 @@
-
 <?php
 include 'conexao.php' ;
 $sql = $pdo->query("SELECT * FROM estoque");
 $dados = $sql->fetchAll(PDO::FETCH_ASSOC) ;
+
+if (isset($_POST["adicionar"])) { 
+    $produto = $_POST["produto"] ;
+    $custo = $_POST["custo"] ;
+    $preco = $_POST["preco"] ;
+    $sql = $pdo->prepare("INSERT INTO estoque  (produto , custo , preco)  VALUES ( ? , ? , ? )");
+    $sql -> execute([$produto, $custo, $preco]);
+    header("Location: index.php");
+    exit;
+}
 ?>
 <!DOCTYPE html>
 <html lang="pt-br">
@@ -10,20 +19,15 @@ $dados = $sql->fetchAll(PDO::FETCH_ASSOC) ;
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Controle de estoque</title>
-    <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Changa+One:ital@0;1&display=swap" rel="stylesheet">
-    <link rel="stylesheet" href="style.css">
 </head>
-<body  class="bg-slate-950">
+<body>
     <header>
-        <div class="text-slate-50"> 
-            <h1 class="changa-one-regular">Controle de Estoque</h1>
+        <div> 
+            <h1>Controle de Estoque</h1>
         </div>
         <div>
-            <h1 class="text-slate-50">Site Feito por:</h1>
-            <h1 class="text-slate-50">Lucas Lemos</h1>
+            <h1>Site Feito por:</h1>
+            <h1>Lucas Lemos</h1>
         </div>
     </header>
     <main> 
@@ -31,9 +35,9 @@ $dados = $sql->fetchAll(PDO::FETCH_ASSOC) ;
             <div>
                 <div>
                     <form method="post">
-                        <input type="text" name="produtos[]" placeholder="Digite o Nome do produto: " required>
-                        <input type="number" name="custos[]"  step="any" placeholder="Digite o custo do produto: " required >
-                        <input type="number" name="precos[]" step="any" placeholder="Digite o preço do produto: " required >
+                        <input type="text" name="produto" placeholder="Digite o Nome do produto: " required>
+                        <input type="number" name="custo"  step="any" placeholder="Digite o custo do produto: " required >
+                        <input type="number" name="preco" step="any" placeholder="Digite o preço do produto: " required >
                         <button type="submit"  name="adicionar"> Adicionar </button>
                 </div>
                 <div>
@@ -48,9 +52,14 @@ $dados = $sql->fetchAll(PDO::FETCH_ASSOC) ;
                             </tr>
                         </thead>
                         <tbody>
+                            <?php  foreach ($dados as $item){ ?>
                             <tr>
-                                <td> </td>
+                                <td><?php echo $item['id'] ;?></td>
+                                <td><?php echo $item['produto'] ;?></td>
+                                <td>R$ <?php echo number_format($item['custo'] , 2 , "," , ".") ;?></td>
+                                <td>R$ <?php echo number_format($item['preco'] , 2 , "," , ".") ;?></td>
                             </tr>
+                            <?php } ?>
                         </tbody>
                     </table>
                 </div>
